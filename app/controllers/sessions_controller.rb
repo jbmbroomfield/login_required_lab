d26fb39c2c4399f9b801a40b71e1f7d443a99f46
jim <jbmbroomfield@gmail.com>
2021-09-25 15:30:26 +1000

    Done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    
+    def new
+    end
+
+    def create
+        name = params[:name]
+        if name && name.length > 0
+            session[:name] = name
+            redirect_to root_path
+        else
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session[:name] = nil
+    end
+
+end
